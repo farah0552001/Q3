d214db023ed14a15678c1d21951b3790542df274
farah0552001 <114291029+farah0552001@users.noreply.github.com>
2022-11-08 22:59:53 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function mooncakeSays (message){
+    let mooncake = '(0.o) ';
+    
+    if (!message){
+       return' ${mooncake) chookity?';
+    }
+    
+       return ' ${mooncake} ${essage)!';
+    }
+    module.exports.mooncakeSays = mooncakeSays;
